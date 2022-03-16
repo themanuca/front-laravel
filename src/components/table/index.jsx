0b7d8fc420206c1accf710ca20d5c20444b042e1
Sergio Manoel <ssergioca@gmail.com>
2022-03-16 12:42:08 -0300

    Front em react

---
+++ src/components/table/index.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Container } from "./styles.js";
+//import Table from 'react-bootstrap/Table';
+import { Table, Button } from 'react-bootstrap';
+
+export default function index() {
+  return (
+  
+        <Table >
+            <thead>
+                <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+                <th>Ação</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>
+                    <Button variant="primary">Primary</Button>{' '}
+                </td>
+                </tr>
+                <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td>
+                    <Button variant="primary">Primary</Button>{' '}
+                </td>
+                </tr>
+                <tr>
+                <td>3</td>
+                
+                <td>@twitter</td>
+                <td>asd</td>
+                <td>asd</td>
+                
+                <td>
+                    <Button variant="primary">Primary</Button>{' '}
+                </td>
+
+                
+
+                </tr>
+            </tbody>
+        </Table>
+    
+  )
+}
